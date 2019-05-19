9f51b33db5b7032f6e7543105868754cae8c3dc4
Zatorska <48672979+Zatorska@users.noreply.github.com>
2019-05-19 18:49:21 +0200

    Delete Czas rozpatrywania wnioskow.sql

--- Czas rozpatrywania wnioskow.sql
+++
@@ -1,8 +0,0 @@
---Czesc wynikow wychodzi ujemnych - dodac komentarz w prezentacji, ze dane sa z bledem :)
-
-select w.id as id_wniosku, w.partner,
-      round((date_part('year',aw.data_zakonczenia)-date_part('year',aw.data_utworzenia))*12 +
-         (date_part('month',aw.data_zakonczenia)-date_part('month',aw.data_utworzenia)) ) as czas_realizacji_w_miesiacach
-from wnioski w
-inner join analizy_wnioskow aw on w.id = aw.id_wniosku
-order by czas_realizacji_w_miesiacach asc
